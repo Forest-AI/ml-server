7bdf265729db9845e88acbfb88ecb8c42e642d3e
Ratan Singh <ratansingh648@gmail.com>
2020-07-28 01:37:03 +0530

    Adding cors to route

--- server/routes/predict.py
+++ server/routes/predict.py
@@ -1,12 +1,15 @@
 from flask import jsonify, request
 from server import app
 import joblib
+from flask_restplus import cors
+
 from server.services.Detector import Detector
 from server.services.Database import Database
 
 detector = Detector() 
 db = Database()
 
+@cors.crossdomain(origin="*")
 @app.route("/predict")
 def predict():
     """Predict route"""
